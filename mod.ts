d06d7ac4b55d6082833002a53bd268a1a1d0f3b9
Coty Hamilton <13724650+cotyhamilton@users.noreply.github.com>
2024-09-21 23:47:43 -0400

    Update settings

--- mod.ts
+++ mod.ts
@@ -1,14 +1,38 @@
 /** zed settings for deno language server */
 export const settings = {
+  lsp: {
+    deno: {
+      settings: {
+        deno: {
+          enable: true,
+        },
+      },
+    },
+  },
   languages: {
     TypeScript: {
-      language_servers: ["deno", "!typescript-language-server", "!vtsls"],
+      language_servers: [
+        "deno",
+        "!typescript-language-server",
+        "!vtsls",
+        "!eslint",
+      ],
     },
     TSX: {
-      language_servers: ["deno", "!typescript-language-server", "!vtsls"],
+      language_servers: [
+        "deno",
+        "!typescript-language-server",
+        "!vtsls",
+        "!eslint",
+      ],
     },
     JavaScript: {
-      language_servers: ["deno", "!typescript-language-server", "!vtsls"],
+      language_servers: [
+        "deno",
+        "!typescript-language-server",
+        "!vtsls",
+        "!eslint",
+      ],
     },
   },
   formatter: "language_server",
