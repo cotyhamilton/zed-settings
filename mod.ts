bc4cbacce7015d45eda902e368b2f9d6ce23112c
Coty Hamilton <13724650+cotyhamilton@users.noreply.github.com>
2024-09-30 19:17:25 -0400

    Add settings to support tailwind via eslint

--- mod.ts
+++ mod.ts
@@ -38,7 +38,57 @@ export const settings = {
   formatter: "language_server",
 };
 
+/** zed settings for tailwind + deno language server */
+export const tailwindSettings = {
+  lsp: {
+    deno: {
+      settings: {
+        deno: {
+          enable: true,
+        },
+      },
+    },
+  },
+  languages: {
+    TypeScript: {
+      language_servers: [
+        "deno",
+        "!typescript-language-server",
+        "!vtsls",
+        "!eslint",
+      ],
+    },
+    TSX: {
+      language_servers: [
+        "deno",
+        "!typescript-language-server",
+        "!vtsls",
+        "eslint",
+      ],
+      code_actions_on_format: {
+        "source.fixAll.eslint": true,
+      },
+    },
+    JavaScript: {
+      language_servers: [
+        "deno",
+        "!typescript-language-server",
+        "!vtsls",
+        "!eslint",
+      ],
+    },
+  },
+  formatter: "language_server",
+};
+
 if (import.meta.main) {
-  console.log(JSON.stringify(settings, null, 2));
+  switch (Deno.args[0]) {
+    case "--tailwind":
+      console.log(JSON.stringify(tailwindSettings, null, 2));
+      break;
+    default:
+      console.log(JSON.stringify(settings, null, 2));
+      break;
+  }
   console.error("%cReload window after saving settings", "color: green;");
 }
